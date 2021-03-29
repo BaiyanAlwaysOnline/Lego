f870eaf37b6a1f1e0e7a629767e97d2b97b97c56
baiyan14514 <baiyan14514@ipalfish.com>
2021-03-29 21:36:42 +0800

    feature: 初始化项目,完成基本结构搭建

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,12 @@
 import { createApp } from 'vue';
 import 'ant-design-vue/dist/antd.css';
-import {
-  Card,
-  Layout,
-  Avatar,
-  Icon,
-} from 'ant-design-vue';
+import Antd from 'ant-design-vue/es';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 createApp(App)
-  .use(Card)
-  .use(Icon)
-  .use(Layout)
-  .use(Avatar)
+  .use(Antd)
   .use(store)
   .use(router)
   .mount('#app');
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,16 @@
+/* eslint-disable import/no-cycle */
 import { createStore } from 'vuex';
+import templates, { TemplatesProps } from '@/store/template';
+import user, { UserProps } from '@/store/user';
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export interface GlobalDataProps {
+  user: UserProps;
+  templates: TemplatesProps;
+}
+
+export default createStore<GlobalDataProps>({
   modules: {
+    templates,
+    user,
   },
 });
